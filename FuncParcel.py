dbeefb4d8362fe4865b8a41ff91b7c2f36fc2ca3
Kai Hwang <kai.hwang@gmail.com>
2015-03-15 08:33:14 +0800

    add python function.

---
+++ FuncParcel.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+Python script for running subcortical parcellation, imported from a python notebook
+"""
+import numpy as np
+import scipy as sp
+import scipy.io as sio
+import matplotlib.pyplot as plt
+import pickle
+import csv

72376afce1ba15dcc73d60656b2065be3c3fc82c
Kai Hwang <kai.hwang@gmail.com>
2015-05-30 23:03:13 -0700

    avoid savetxt missing 0...

--- sge_modularity.py
+++ sge_modularity.py
@@ -7,7 +7,7 @@ import numpy as np
 subject = sys.stdin.read().strip('\n')
 print(subject)
 print(type(subject))
-ci, q = FuncParcel.iterate_modularity_partition(subject)
+ci, q = FuncParcel.iterate_modularity_partition(subject, 100)
 fn = '/home/despoB/kaihwang/bin/FuncParcel/Data/Subject_Partition/%s_ci' %subject
 ci = ci +1
 np.savetxt(fn, ci, fmt='%3.d') 

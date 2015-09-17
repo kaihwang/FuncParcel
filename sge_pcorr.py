704f6eb47176a113b8bcfbe7067c9071fcaa6e17
kai.hwang@gmail.com <kaihwang@nx5.priv>
2015-09-17 16:19:33 -0700

    for sge.

---
+++ sge_pcorr.py
@@ -0,0 +1,24 @@
+import sys
+from FuncParcel import *
+
+# script to do partial corr
+
+subject = sys.stdin.read().strip('\n')
+
+ts_path = '/home/despoB/connectome-thalamus/NotBackedUp/TS/'
+
+pcorr_path = '/home/despoB/connectome-thalamus/NotBackedUp/ParMatrices/'
+
+fn = ts_path + subject + 'Thalamus_indices_TS_000.netts'
+subcortical_ts = np.loadtxt(fn)
+
+ROIs = ['Cortical_CI', 'Cortical_ROIs', 'Craddock_300_cortical']
+for roi in ROIs:
+	fn = ts_path + subject + roi + '_TS_000.netts' 
+	cortical_ts = np.loadtxt(fn)
+
+	pcorr_mat = pcorr_subcortico_cortical_connectivity(subcortical_ts, cortical_ts)
+
+	fn = pcorr_path + subject + roi + '_pcorr_mat'
+	np.savetxt(fn, pcorr_mat, fmt='%.4f')
+

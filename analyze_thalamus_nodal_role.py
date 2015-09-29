aa80e20481c3dd270206d5561fc1a030230fa67b
kai.hwang@gmail.com <kaihwang@nx3.priv>
2015-09-28 22:36:45 -0700

    fix WMD.

--- analyze_thalamus_nodal_role.py
+++ analyze_thalamus_nodal_role.py
@@ -145,17 +145,115 @@ save_object(Tha_BNWR, path_to_data_folder +'/Tha_BNWR')
 
 
 #get cortical PC and BNEW and WMD
+Cortical_adj = np.loadtxt('/home/despoB/connectome-thalamus/AvgMatrices/MGH/_Craddock_300_cortical_corrmatavg')
+Cortical_adj[np.isnan(Cortical_adj)] = 0
+
+Cortical_wm_mean = {}
+Cortical_wm_std = {}
+Cortical_PCs = np.zeros(Cortical_CI.size)
+Cortical_WMDs = np.zeros(Cortical_CI.size)
+Cortical_BNWR = np.empty(Cortical_CI.size)
+for ix, c in enumerate(np.arange(0.01,0.16, 0.01)):
+
+	#pc
+	#graph = matrix_to_igraph(Cortical_adj.copy(),cost=c)
+	#nodal_graph = brain_graph(VertexClustering(graph, membership=Cortical_CI))
+	Cortical_PCs += bct.participation_coef(bct.threshold_proportional(Cortical_adj, c, copy=True), Cortical_CI)
+
+	#wmd
+	M = bct.weight_conversion(bct.threshold_proportional(Cortical_adj, c, copy=True), 'binarize')
+	Cortical_WMDs += bct.module_degree_zscore(M, Cortical_CI)
+	#return mean and degree 
+	for CI in np.unique(Cortical_CI):
+		Cortical_wm_mean[ix+1, CI] = np.nanmean(np.sum(M[Cortical_CI==CI,:],1))
+		Cortical_wm_std[ix+1, CI] = np.nanstd(np.sum(M[Cortical_CI==CI,:],1))
+
+	#BNWR
+	M = bct.threshold_proportional(Cortical_adj, c, copy=True)
+	BNWR = np.zeros(Cortical_CI.size)
+	for i in range(len(Cortical_CI)):
+		sum_between_weight = np.nansum(M[i,Cortical_CI!=Cortical_CI[i]])
+		sum_total = np.nansum(M[i,:])
+		BNWR[i] = sum_between_weight / sum_total
+		BNWR[i] = np.nan_to_num(BNWR[i])
+		#sBNWR = np.concatenate(BNWR,sBNWR)
+	Cortical_BNWR += BNWR	
 
 
+Cortical_PCs_percentage = (Cortical_PCs/13.5)*100 #13.5 is the highest PC possible  (upper bound is .9 for each cost, .9*15 = 13.5)
+atlas_path = path_to_ROIs+'/Craddock_300_cortical.nii.gz' 
+image_path = '/home/despoB/connectome-thalamus/Thalamic_parcel/MGH_cortical_nodal_role_pc.nii.gz'
+make_image(atlas_path, image_path, Cortical_ROIs, Cortical_PCs_percentage)
+save_object(Cortical_PCs, path_to_data_folder +'/Cortical_PCs')
+
+Cortical_WMDs_percentage = (Cortical_WMDs/15)*100 
+image_path = '/home/despoB/connectome-thalamus/Thalamic_parcel/MGH_cortical_nodal_role_wmd.nii.gz'
+make_image(atlas_path, image_path, Cortical_ROIs, Cortical_WMDs_percentage)
+save_object(Cortical_WMDs, path_to_data_folder +'/Cortical_WMDs')
+
+Cortical_BNWR_percentage = (Cortical_BNWR/15)*100 
+image_path = '/home/despoB/connectome-thalamus/Thalamic_parcel/MGH_cortical_nodal_role_BNWR.nii.gz'
+make_image(atlas_path, image_path, Cortical_ROIs, Cortical_BNWR_percentage)
+save_object(Cortical_BNWR, path_to_data_folder +'/Cortical_BNWR')
 
 #get thlamus WMD, using mean and STD from cortex. 
+Tha_WMDs = np.empty(Cortical_plus_thalamus_CI.size)
+for ix, c in enumerate(cost_thresholds):
+	Par_adj = Thalamocortical_corrmat.copy()
+	Par_adj[Cortical_ROIs_positions[Cortical_CI==0],:]=0
+	Par_adj[:,Cortical_ROIs_positions[Cortical_CI==0]]=0
+	Par_adj[Par_adj<c]=0
+	M = bct.weight_conversion(Par_adj, 'binarize')
+
+	tha_wmd = np.zeros(Cortical_plus_thalamus_CI.size)
+	for i in np.unique(Cortical_CI):
+		tha_wmd[Cortical_plus_thalamus_CI==i] = (np.sum(M[Cortical_plus_thalamus_CI==i,0:320],1) - Cortical_wm_mean[ix+1,i])/Cortical_wm_std[ix+1,i]
+	tha_wmd = np.nan_to_num(tha_wmd)
+	Tha_WMDs += tha_wmd
+	#graph = matrix_to_igraph(Par_adj.copy(),cost=1)
+	#nodal_graph = brain_graph(VertexClustering(graph, membership=Cortical_plus_thalamus_CI))
+	#save_path = '/home/despoB/kaihwang/bin/FuncParcel/Data/Graph/graph_MGH_%s' %roi_template
+	#save_object(nodal_graph, save_path)
+
+Tha_WMDs[Cortical_ROIs_positions.astype('int')]=0
+Tha_WMDs_percentage = (Tha_WMDs/15)*100 #13.5 is the highest PC possible  (upper bound is .9 for each cost, .9*15 = 13.5)
+atlas_path = path_to_ROIs+'/Craddock_300_plus_thalamus_ROIs.nii.gz' 
+image_path = '/home/despoB/connectome-thalamus/Thalamic_parcel/MGH_tha_nodal_role_WMD.nii.gz'
+make_image(atlas_path, image_path, ROIs, Tha_WMDs_percentage)
+save_object(Tha_WMDs, path_to_data_folder +'/Tha_WMDs')
 
 
+#get number of networks/communities connected
+NNCs = np.empty(Cortical_plus_thalamus_CI.size)
+for ic, c in enumerate(cost_thresholds):
+	Par_adj = Thalamocortical_corrmat.copy()
+	#Par_adj[Cortical_ROIs_positions[Cortical_CI==0],:]=0
+	#Par_adj[:,Cortical_ROIs_positions[Cortical_CI==0]]=0
+	Par_adj[Par_adj<c]=0
 
-###### calculate cortical node metrics
-#cortical_adj = adj[0:320,0:320].copy()
-#graph = matrix_to_igraph(cortical_adj,cost=0.15)
-#cortical_graph = brain_graph(VertexClustering(graph, membership=Cortical_CI))
+	Tha_NNCs = np.zeros(Cortical_plus_thalamus_CI.size)
+	for ix, i in enumerate(Thalamus_voxel_positions):
+		Tha_NNCs[i] = len(np.unique(Cortical_plus_thalamus_CI[Par_adj[i,]!=0]))
+
+	NNCs += Tha_NNCs
+
+
+Cortical_NNCs = np.empty(Cortical_plus_thalamus_CI.size)
+for ic, c in enumerate(np.arange(0.01,0.16, 0.01)):
+	M= bct.threshold_proportional(Cortical_adj, c, copy=True)
+
+	Cor_NNCs = np.zeros(Cortical_plus_thalamus_CI.size)
+	for ix, i in enumerate(Cortical_ROIs_positions):
+		Cor_NNCs[i] = len(np.unique(Cortical_plus_thalamus_CI[M[i,]!=0]))
+
+	Cortical_NNCs += Cor_NNCs
+NNCs[Cortical_ROIs_positions] = Cortical_NNCs[Cortical_ROIs_positions] 
+
+NNCs_percentage = (NNCs/15)*100 #13.5 is the highest PC possible  (upper bound is .9 for each cost, .9*15 = 13.5)
+atlas_path = path_to_ROIs+'/Craddock_300_plus_thalamus_ROIs.nii.gz' 
+image_path = '/home/despoB/connectome-thalamus/Thalamic_parcel/MGH_cortex_tha_nodal_role_NNC.nii.gz'
+make_image(atlas_path, image_path, ROIs, NNCs_percentage)
+save_object(NNCs, path_to_data_folder +'/NNCs')
 
 #### get descriptive stats of PC within parcel
 mean_PC_per_thalamus_parcel = np.zeros(max(Thalamus_CIs))

bf9bc55e1529f65672136f6263e244c521cbbd97
kai.hwang@gmail.com <kaihwang@nx3.priv>
2015-09-29 10:23:45 -0700

    restart lesion analysis.

--- analyze_thalamus_nodal_role.py
+++ analyze_thalamus_nodal_role.py
@@ -4,7 +4,11 @@ from brain_graphs import *
 from FuncParcel import *
 import matplotlib.pylab as plt
 
+
+################################################################
 ###### setup
+################################################################
+
 AvgMat_path = '/home/despoB/connectome-thalamus/AvgMatrices'
 Parcel_path = '/home/despoB/connectome-thalamus/Thalamic_parcel'
 roi_template = 'Craddock_300_plus_thalamus_ROIs_ncsreg' 
@@ -21,7 +25,12 @@ Thalamocortical_corrmat = np.loadtxt(Parcel_path+'/MGH_Craddock_300_cortical_plu
 
 Cortical_CI = np.loadtxt(path_to_ROIs+'/Cortical_CI', dtype='int')
 
+
+
+################################################################
 ###### find cortical target and nontarget, save output 
+################################################################
+
 Cortical_targets, Cortical_nontargets, cost_thresholds = map_subcortical_cortical_targets(Thalamocortical_corrmat, Cortical_ROIs, Thalamus_voxels)
 save_object(Cortical_targets, path_to_data_folder +'/Cortical_targets')
 save_object(Cortical_nontargets, path_to_data_folder +'/Cortical_nontargets')
@@ -41,8 +50,10 @@ atlas_path = path_to_ROIs+'/Craddock_300_cortical.nii.gz'
 image_path = Parcel_path +'/MGH_cortical_roi_target_count.nii.gz' 
 make_image(atlas_path, image_path, Cortical_ROIs, Cortical_ROI_target_count)
 
-###### do network based parcellation
 
+################################################################
+###### do network based parcellation
+################################################################
 
 #call function
 Netadjmat = np.loadtxt('/home/despoB/connectome-thalamus/Thalamic_parcel/MGH_cortical_network_plus_thalamus_parcorrmatavg')
@@ -52,10 +63,10 @@ Cortical_Network_CIs_plus_thalamus = np.loadtxt('/home/despoB/connectome-thalamu
 
 
 #clip out bad SNR networks (orbital frontal and inf temporal)
+Netadjmat[8,:]=0
+Netadjmat[:,8]=0
 Netadjmat[9,:]=0
 Netadjmat[:,9]=0
-Netadjmat[10,:]=0
-Netadjmat[:,10]=0
 
 #call function
 _, Thalamo_ParcelCIs, _ = parcel_subcortical_network(Netadjmat, Cortical_Network_CIs_plus_thalamus, Thalamus_voxels, Cortical_Network_CIs, Cortical_Network_CIs)
@@ -65,12 +76,12 @@ save_object(Thalamo_ParcelCIs, path_to_data_folder +'/Thalamo_ParcelCIs')
 #sort CI vector
 Thalamus_CIs = np.zeros(len(Thalamus_voxel_coordinate))
 for i, thalamus_voxel_index in enumerate(Thalamus_voxel_coordinate[:,3]):
-	Thalamus_CIs[i]= Thalamo_ParcelCIs[thalamus_voxel_index][0]
+	Thalamus_CIs[i] = Thalamo_ParcelCIs[thalamus_voxel_index][0]
 
 # show histogram of CI distribution
 Thalamus_CIs = Thalamus_CIs.astype(int)
-plt.hist(Thalamus_CIs, bins=9)
-#plt.show()
+plt.hist(Thalamus_CIs, 11)
+plt.show()
 
 #write out parcel info into nifti
 atlas_path = path_to_ROIs+'/Thalamus_indices.nii.gz' 
@@ -78,23 +89,12 @@ ROI_list = path_to_ROIs + '/thalamus_voxel_indices'
 image_path = '/home/despoB/connectome-thalamus/Thalamic_parcel/MGH_network_based_thalamus_parcel.nii.gz' 
 make_image(atlas_path, image_path, ROI_list, Thalamus_CIs)
 
+
+
+
+################################################################
 ###### Here analyze each thalamic voxel's nodal property
-#first load thalamus voxel + cortical ROI's matrix, there are two. One partial, one full correlation
-#path = AvgMat_path + 'MGH/_Craddock_300_plus_thalamus_ROIs_ncsreg_corrmatavg' 
-#Full_adj = np.loadtxt(path)
-#Full_adj[Full_adj<0] = 0.0
-#np.fill_diagonal(Full_adj,0)
-
-
-#clip within thalamus edges
-#adj[320:3859,320:3859] = 0.0
-
-# load cortical CI vectors
-#partition = pickle.load(open(path_to_data_folder+"/Graph/graph_MGH_Craddock_300_cortical_min_cost0.02_min_weight2.0_min_size5", "rb"))
-#Cortical_CI=np.array(partition.community.membership) +1
-#Cortical_CI[Cortical_CI>10] = 0
-
-#Thalamus_CIs = np.zeros(len(Thalamus_voxel_coordinate))
+################################################################
 Cortical_plus_thalamus_CI = np.concatenate((Cortical_CI, Thalamus_CIs), 1) 
 Cortical_plus_thalamus_CI = Cortical_plus_thalamus_CI.astype(int)
 
@@ -255,25 +255,26 @@ image_path = '/home/despoB/connectome-thalamus/Thalamic_parcel/MGH_cortex_tha_no
 make_image(atlas_path, image_path, ROIs, NNCs_percentage)
 save_object(NNCs, path_to_data_folder +'/NNCs')
 
+
+################################################################
 #### get descriptive stats of PC within parcel
+################################################################
 mean_PC_per_thalamus_parcel = np.zeros(max(Thalamus_CIs))
-for ix, i in enumerate(np.arange(1,max(Thalamus_CIs)+1,1)):
-	mean_PC_per_thalamus_parcel[ix] = np.nanmean(Thalamus_pc[Thalamus_CIs==i])
+for ix, i in enumerate(np.unique(Thalamus_CIs)):
+	mean_PC_per_thalamus_parcel[ix] = np.nanmean(Tha_PCs_percentage[Thalamus_CIs==i])
 
-#get ratio of between/total connectivity weight
-adjt = bct.threshold_proportional(adj,0.015)
+mean_WMD_per_thalamus_parcel = np.zeros(max(Thalamus_CIs))
+for ix, i in enumerate(np.unique(Thalamus_CIs)):
+	mean_WMD_per_thalamus_parcel[ix] = np.nanmean(Tha_WMDs_percentage[Thalamus_CIs==i])	
 
+mean_BNWR_per_thalamus_parcel = np.zeros(max(Thalamus_CIs))
+for ix, i in enumerate(np.unique(Thalamus_CIs)):
+	mean_BNWR_per_thalamus_parcel[ix] = np.nanmean(Tha_BNWR_percentage[Thalamus_CIs==i])
 
-
-mean_ratio_per_thalamus_parcel = np.zeros(max(Thalamus_CIs))	
-for ix, i in enumerate(np.arange(1,max(Thalamus_CIs)+1,1)):
-	mean_ratio_per_thalamus_parcel[ix] = np.nanmean(thalamus_between_ratio[Thalamus_CIs==i])
-
-#write to nifti
-thalamus_between_ratio = thalamus_between_ratio*100
-atlas_path = path_to_ROIs+'/Thalamus_indices.nii.gz' 
-image_path = '/home/despoB/connectome-thalamus/Thalamic_parcel/MGH_%s_nodal_role_bcr.nii.gz' %roi_template
-make_image(atlas_path, image_path, ROI_list, pc.astype(int))
+Tha_NNCs = NNCs_percentage[Thalamus_voxel_positions]/100		
+mean_NNCs_per_thalamus_parcel = np.zeros(max(Thalamus_CIs))
+for ix, i in enumerate(np.unique(Thalamus_CIs)):
+	mean_NNCs_per_thalamus_parcel[ix] = np.nanmean(Tha_NNCs[Thalamus_CIs==i])
 
 
 

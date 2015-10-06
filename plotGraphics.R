c3586e58f6b450ccf19261d76c60738f3883ee97
kai.hwang@gmail.com <kaihwang@nx3.priv>
2015-10-06 16:41:46 -0700

    visualize bcc results.

--- plotGraphics.R
+++ plotGraphics.R
@@ -9,19 +9,23 @@ library(reshape2)
 
 Thalamus_Data = read.csv('Thalamus_nodal.csv', header=TRUE)
 Thalamus_Data <- Thalamus_Data[Thalamus_Data$PC<1,]  
-Thalamus_Data <- Thalamus_Data[Thalamus_Data$target_PC<20,]
+#Thalamus_Data <- Thalamus_Data[Thalamus_Data$target_PC<20,]
 
 Cortical_Data = read.csv('Cortical_nodal.csv', header=TRUE)
 Cortical_Data <- Cortical_Data[Cortical_Data$PC>0,]  
 Cortical_Data <- Cortical_Data[Cortical_Data$Associated.System!='Other',] 
 
+CI_colors <- c("#008080", "purple", "green", "red", "yellow", "black", "cyan", "pink", "blue")
+
 ### boxplot to compare nodal roles between each partition, for thalamus and cortex
-Variables_to_plot <- c('PC', 'WMD', 'NNC', 'BNWR')
+Variables_to_plot <- c('PC', 'WMD', 'NNC', 'BNWR', 'bcc')
 for (v in Variables_to_plot){
-  Thalamus_boxplot <- ggplot(data = Thalamus_Data, aes_string(x="Associated.System", y=v, fill="Associated.System")) + geom_boxplot() #+geom_point() + geom_jitter(position = position_jitter(width = .1))
+  Thalamus_boxplot <- ggplot(data = Thalamus_Data, aes_string(x="Associated.System", y=v, fill="Associated.System", colour="Associated.System")) + geom_boxplot(outlier.colour = NULL) #+geom_point() + geom_jitter(position = position_jitter(width = .1))
+  Thalamus_boxplot <- Thalamus_boxplot + ggtitle(paste("Thalamus", v)) + scale_fill_manual(values=CI_colors)  + theme_grey(base_size = 32)  + theme(axis.text.x=element_text(angle=90, hjust=1))
   plot(Thalamus_boxplot)
 
-  Cortical_boxplot <- ggplot(data = Cortical_Data, aes_string(x="Associated.System", y=v, fill="Associated.System")) + geom_boxplot() #+geom_point() + geom_jitter(position = position_jitter(width = .1))
+  Cortical_boxplot <- ggplot(data = Cortical_Data, aes_string(x="Associated.System", y=v, fill="Associated.System", colour="Associated.System")) + geom_boxplot(outlier.colour = NULL) #+geom_point() + geom_jitter(position = position_jitter(width = .1))
+  Cortical_boxplot <- Cortical_boxplot + ggtitle(paste("Cortical", v))+ scale_fill_manual(values=CI_colors) + theme_grey(base_size = 32)  + theme(axis.text.x=element_text(angle=90, hjust=1))
   plot(Cortical_boxplot)
 }
 
@@ -40,7 +44,7 @@ plot(Thalamus_pc_plot)
 Thalamus_Target_Data = read.csv('Thalamus_target.csv', header=TRUE)
 total <- merge(Thalamus_Target_Data,Thalamus_Data,by="Voxel")
 
-Thalamus_pc_plot <-ggplot(data = total, aes(x=Associated.System.y , y=Target.PC))+geom_point() + geom_jitter(position = position_jitter(width = .1))
+Thalamus_pc_plot <-ggplot(data = total, aes(x=Associated.System.y , y=Target.PC, color =Associated.System.y ))+geom_point() + geom_jitter(position = position_jitter(width = .1)) +  scale_colour_manual(values=CI_colors) 
 plot(Thalamus_pc_plot)
 
 
@@ -61,7 +65,7 @@ cScale <- c("#e7298a",
 "#386cb0")
 patient_data = read.csv('patient_df.csv', header=TRUE)
 
-plot_data = melt(patient_data, id_vars = c("SubjID", "Voxel", "CI", "PC"), measure.vars = c("Target_total_weight_wn", "nonTarget_total_weight_wn"))
+plot_data = melt(patient_data, id_vars = c("SubjID", "Voxel", "CI", "PC"), measure.vars = c("Target_connected_weight", "nonTarget_connected_weight"))
 
 patient_plot <-ggplot(plot_data , aes(x=PC , y=value, colour=variable))+geom_point(size=3, aes(shape = factor(SubjID)))+ scale_colour_manual(values=c("Red", "Black")) + facet_grid(.~CI)
                                                                                                                                             

4318ff76ad38035ef5eb8c2cd38132878bc7b75a
Kai Hwang <kai.hwang@gmail.com>
2016-03-21 11:17:41 -0700

    plot cognitive components

--- plotGraphics.R
+++ plotGraphics.R
@@ -11,20 +11,52 @@ Thalamus_Data = read.csv('Thalamus_nodal_WTA.csv', header=TRUE)
 Cortical_Data = read.csv('Cortical_nodal_WTA.csv', header=TRUE)
 Cortical_Data <- Cortical_Data[Cortical_Data$Functional.Network!='Other',] 
 
+Thalamus_plus_cortical_data <- read.csv('Cortical_plus_thalamus_nodal_WTA.csv', header=TRUE)
+Thalamus_plus_cortical_data <- Thalamus_plus_cortical_data[Thalamus_plus_cortical_data$Classification!='Unclassified',] 
+Thalamus_Data <- Thalamus_Data[Thalamus_Data$Morel.Parcellations!='Unclassified',] 
+
+### plot summary graph
+X_order <- c('First Order \nThalamic Nuclei','Higher Order \nThalamic Nuclei','Nonspecific \nThalamic Nuclei',"Cortical \nConnector Hubs", "Cortical \nProvincial Hubs", "Cortical \nNone Hubs")
+Thalamus_plus_cortical_data$Classification <-factor(Thalamus_plus_cortical_data$Classification, levels=X_order)
+
+#PC
+volplot <- ggplot(data = Thalamus_plus_cortical_data, aes(x=PC))
+volplot <- volplot + stat_density(aes(ymax = ..density..,  ymin = -..density.. ),geom = "ribbon", position = "identity" )
+volplot <- volplot + facet_grid(. ~ Classification) + coord_flip() + theme_grey(base_size = 10)
+volplot <- volplot  + xlim(0,1) + theme( axis.title.x=element_blank(),axis.ticks.x=element_blank(),axis.text.x=element_blank(), axis.text = element_text(colour = "black")) 
+plot(volplot)
+ggsave(filename = 'PC_classification.pdf', plot = volplot, units = c("in"),width=6.5, height=1.7,dpi=300) 
+
+#WMD
+volplot <- ggplot(data = Thalamus_plus_cortical_data, aes(x=WMD))
+volplot <- volplot + stat_density(aes(ymax = ..density..,  ymin = -..density.. ),geom = "ribbon", position = "identity" )
+volplot <- volplot + facet_grid(. ~ Classification) + coord_flip() + theme_grey(base_size = 10) 
+volplot <- volplot  + xlim(-3,4.5) + theme( axis.title.x=element_blank(),axis.ticks.x=element_blank(),axis.text.x=element_blank(), axis.text = element_text(colour = "black")) 
+plot(volplot)
+ggsave(filename = 'WMD_classification.pdf', plot = volplot, units = c("in"),width=6.5, height=1.7,dpi=300) 
+
+#CogFlex
+volplot <- ggplot(data = Thalamus_plus_cortical_data, aes(x=cog))
+volplot <- volplot + stat_density(aes(ymax = ..density..,  ymin = -..density.. ),geom = "ribbon", position = "identity" )
+volplot <- volplot + facet_grid(. ~ Classification) + coord_flip() + theme_grey(base_size = 10) 
+volplot <- volplot  + xlim(0,8) + theme( axis.title.x=element_blank(),axis.ticks.x=element_blank(),axis.text.x=element_blank(), axis.text = element_text(colour = "black")) 
+plot(volplot)
+ggsave(filename = 'Cog_classification.pdf', plot = volplot, units = c("in"),width=6.5, height=1.7,dpi=300) 
+
+
 Nuclei_order <-c('AN', 'LD', 'MD', 'CL', 'CeM', 'CM', 'Pf', 'Li', 'PuA', 'PuI', 'PuL','PuM','LP','Po','SG','MGN','LGN','VA','VL','VM','VPI','VPL','VPM' )
 Thalamus_Data$Morel.Parcellations_f = factor(Thalamus_Data$Morel.Parcellations, levels=Nuclei_order)
-
 #CI_colors <- c("#008080", "purple", "green", "red", "yellow", "magenta", "cyan", "pink", "blue", "pink")
 
 ### volcano plot
-Variables_to_plot <- c('WMD'  ) #'NNC', 'BNWR', 'bcc' 'WMD'
+Variables_to_plot <- c('PC'  ) #'NNC', 'BNWR', 'bcc' 'WMD'
 
 for (v in Variables_to_plot){
   
   volplot <- ggplot(data = Thalamus_Data, aes_string(x=v))
   volplot <- volplot + stat_density(aes(ymax = ..density..,  ymin = -..density.. ),geom = "ribbon", position = "identity" )
   volplot <- volplot + facet_grid(. ~ Functional.Network) + coord_flip() + theme_grey(base_size = 10) 
-  volplot <- volplot  + xlim(-2,2) + theme( axis.title.x=element_blank(),axis.ticks.x=element_blank(),axis.text.x=element_blank(), axis.text = element_text(colour = "black")) 
+  volplot <- volplot  + xlim(0,1) + theme( axis.title.x=element_blank(),axis.ticks.x=element_blank(),axis.text.x=element_blank(), axis.text = element_text(colour = "black")) 
   volplot <- volplot + ggtitle("Thalamus Functional Atlas")
   plot(volplot)
   ggsave(filename = paste(v,'_tha_fn_density.pdf', sep=''), plot = volplot, units = c("in"),width=3.4, height=1.5) 
@@ -32,7 +64,7 @@ for (v in Variables_to_plot){
   volplot <- ggplot(data = Cortical_Data, aes_string(x=v))
   volplot <- volplot + stat_density(aes(ymax = ..density..,  ymin = -..density.. ),geom = "ribbon", position = "identity" )
   volplot <- volplot + facet_grid(. ~ Functional.Network) + coord_flip() + theme_grey(base_size = 10) 
-  volplot <- volplot  + xlim( 0,9) + theme( axis.title.x=element_blank(),axis.ticks.x=element_blank(),axis.text.x=element_blank(), axis.text = element_text(colour = "black")) 
+  volplot <- volplot  + xlim( 0,1) + theme( axis.title.x=element_blank(),axis.ticks.x=element_blank(),axis.text.x=element_blank(), axis.text = element_text(colour = "black")) 
   volplot <- volplot + ggtitle("Cortical ROI")
   plot(volplot)
   ggsave(filename = paste(v,'_cortical_fn_density.pdf', sep=''), plot = volplot, units = c("in"),width=3.4, height=1.5) 
@@ -40,7 +72,7 @@ for (v in Variables_to_plot){
   volplot <- ggplot(data = Thalamus_Data, aes_string(x=v))
   volplot <- volplot + stat_density(aes(ymax = ..density..,  ymin = -..density.. ),geom = "ribbon", position = "identity" )
   volplot <- volplot + facet_grid(. ~ Anatomical.Parcellations) + coord_flip() + theme_grey(base_size = 10) 
-  volplot <- volplot  + xlim( 0,9) +  theme( axis.title.x=element_blank(),axis.ticks.x=element_blank(),axis.text.x=element_blank(), axis.text = element_text(colour = "black")) 
+  volplot <- volplot  + xlim( 0,1) +  theme( axis.title.x=element_blank(),axis.ticks.x=element_blank(),axis.text.x=element_blank(), axis.text = element_text(colour = "black")) 
   volplot <- volplot + ggtitle("Thalamus Oxford-FSL Atlas")
   plot(volplot)
   ggsave(filename = paste(v,'_thalamus_an_density.pdf', sep=''), plot = volplot, units = c("in"),width=2.85, height=1.5) 
@@ -49,101 +81,12 @@ for (v in Variables_to_plot){
   volplot <- ggplot(data = Thalamus_Data, aes_string(x=v))
   volplot <- volplot + stat_density(aes(ymax = ..density..,  ymin = -..density.. ),geom = "ribbon", position = "identity" )
   volplot <- volplot + facet_wrap(~Morel.Parcellations_f, ncol = 8) + coord_flip() + theme_grey(base_size = 10) 
-  volplot <- volplot  + xlim( 0,9) +theme( axis.title.x=element_blank(),axis.ticks.x=element_blank(),axis.text.x=element_blank(), axis.text = element_text(colour = "black")) 
+  volplot <- volplot  + xlim( 0,1) +theme( axis.title.x=element_blank(),axis.ticks.x=element_blank(),axis.text.x=element_blank(), axis.text = element_text(colour = "black")) 
   volplot <- volplot + ggtitle("Thalamus Morel Atlas") + theme(panel.margin.y = unit(1.25, "cm"))
   plot(volplot)
   ggsave(filename = paste(v,'_thalamus_morel_density.pdf', sep=''), plot = volplot, units = c("in"),width=3.4, height=4.25) 
   
 }  
-
-### boxplot to compare nodal roles between each partition, for thalamus and cortex
-
-
-for (v in Variables_to_plot){
-  
-  Thalamus_boxplot <- ggplot(data = Thalamus_Data, aes_string(x="Functional.Network", y=v)) + geom_boxplot(outlier.colour = NULL,fill = "grey80") #+geom_point() + geom_jitter(position = position_jitter(width = .1))
-  Thalamus_boxplot <- Thalamus_boxplot + ggtitle(paste("Functional Parcellation", v))  + theme_grey(base_size = 10)  + theme( axis.title.x=element_blank(), axis.title.y=element_blank(), axis.ticks.y=element_blank(),axis.text.y=element_blank(),legend.position="none", axis.text = element_text(colour = "black"))
-  Thalamus_boxplot <- Thalamus_boxplot + ylim( 0, 1) 
-  plot(Thalamus_boxplot)
-  ggsave(filename = paste(v,'_tha_fn.pdf', sep=''), plot = Thalamus_boxplot, units = c("in"),width=2.5, height=2.5) 
-
-  Cortical_boxplot <- ggplot(data = Cortical_Data, aes_string(x="Functional.Network", y=v)) + geom_boxplot(outlier.colour = NULL, ,fill = "grey80") #+geom_point() + geom_jitter(position = position_jitter(width = .1))
-  Cortical_boxplot <- Cortical_boxplot + ggtitle(paste("Cortical ROI", v)) + theme_grey(base_size = 10) +  theme( axis.title.x=element_blank(), legend.position="none", axis.text = element_text(colour = "black"))
-  Cortical_boxplot <- Cortical_boxplot + ylim( 0, 1)
-  plot(Cortical_boxplot)
-  ggsave(filename = paste(v,'_cortical_box_consensus.pdf', sep=''), plot = Cortical_boxplot, units = c("in"),width=3, height=2.5) 
-  
-  Thalamus_boxplot <- ggplot(data = Thalamus_Data, aes_string(x="Anatomical.Parcellations", y=v)) + geom_boxplot(outlier.colour = NULL,fill = "grey80") #+geom_point() + geom_jitter(position = position_jitter(width = .1))
-  Thalamus_boxplot <- Thalamus_boxplot + ggtitle(paste("Oxford-FSL Atlas", v))  + theme_grey(base_size = 10)  + theme( axis.title.x=element_blank(), axis.title.y=element_blank(),axis.ticks.y=element_blank(), axis.text.y=element_blank(),legend.position="none", axis.text = element_text(colour = "black"))
-  Thalamus_boxplot <- Thalamus_boxplot + ylim( 0, 1) 
-  plot(Thalamus_boxplot)
-  ggsave(filename = paste(v,'_fsl_fn.pdf', sep=''), plot = Thalamus_boxplot, units = c("in"),width=2, height=2.5) 
-  
-  Thalamus_Data <- Thalamus_Data[Thalamus_Data$Morel.Parcellations!='Unclassified',] 
-  Thalamus_boxplot <- ggplot(data = Thalamus_Data, aes_string(x="Morel.Parcellations", y=v)) + geom_boxplot(outlier.colour = NULL,fill = "grey80") #+geom_point() + geom_jitter(position = position_jitter(width = .1))
-  Thalamus_boxplot <- Thalamus_boxplot + ggtitle(paste("Morel Atlas", v))  + theme_grey(base_size = 10)  + theme( axis.title.x=element_blank(), legend.position="none", axis.text = element_text(colour = "black"))
-  Thalamus_boxplot <- Thalamus_boxplot + ylim( 0, 1) 
-  plot(Thalamus_boxplot)
-  ggsave(filename = paste(v,'_morel_fn.png', sep=''), plot = Thalamus_boxplot, units = c("in"),width=6, height=2.5) 
-}
-
-Variables_to_plot <- c('BNWR'  )
-for (v in Variables_to_plot){
-  
-  Thalamus_boxplot <- ggplot(data = Thalamus_Data, aes_string(x="Functional.Network", y=v)) + geom_boxplot(outlier.colour = NULL,fill = "grey80") #+geom_point() + geom_jitter(position = position_jitter(width = .1))
-  Thalamus_boxplot <- Thalamus_boxplot + ggtitle(paste("Functional Parcellation", v))  + theme_grey(base_size = 10)  + theme( axis.title.x=element_blank(), axis.title.y=element_blank(), axis.ticks.y=element_blank(),axis.text.y=element_blank(),legend.position="none", axis.text = element_text(colour = "black"))
-  Thalamus_boxplot <- Thalamus_boxplot + ylim( 0, 1) 
-  plot(Thalamus_boxplot)
-  ggsave(filename = paste(v,'_tha_fn.pdf', sep=''), plot = Thalamus_boxplot, units = c("in"),width=2.5, height=2.5) 
-  
-  Cortical_boxplot <- ggplot(data = Cortical_Data, aes_string(x="Functional.Network", y=v)) + geom_boxplot(outlier.colour = NULL, ,fill = "grey80") #+geom_point() + geom_jitter(position = position_jitter(width = .1))
-  Cortical_boxplot <- Cortical_boxplot + ggtitle(paste("Cortical ROI", v)) + theme_grey(base_size = 10) +  theme( axis.title.x=element_blank(), legend.position="none", axis.text = element_text(colour = "black"))
-  Cortical_boxplot <- Cortical_boxplot + ylim( 0, 1)
-  plot(Cortical_boxplot)
-  ggsave(filename = paste(v,'_cortical_box_consensus.pdf', sep=''), plot = Cortical_boxplot, units = c("in"),width=3, height=2.5) 
-  
-  Thalamus_boxplot <- ggplot(data = Thalamus_Data, aes_string(x="Anatomical.Parcellations", y=v)) + geom_boxplot(outlier.colour = NULL,fill = "grey80") #+geom_point() + geom_jitter(position = position_jitter(width = .1))
-  Thalamus_boxplot <- Thalamus_boxplot + ggtitle(paste("Oxford-FSL Atlas", v))  + theme_grey(base_size = 10)  + theme( axis.title.x=element_blank(), axis.title.y=element_blank(),axis.ticks.y=element_blank(), axis.text.y=element_blank(),legend.position="none", axis.text = element_text(colour = "black"))
-  Thalamus_boxplot <- Thalamus_boxplot + ylim( 0, 1) 
-  plot(Thalamus_boxplot)
-  ggsave(filename = paste(v,'_fsl_fn.pdf', sep=''), plot = Thalamus_boxplot, units = c("in"),width=2, height=2.5) 
-  
-  Thalamus_Data <- Thalamus_Data[Thalamus_Data$Morel.Parcellations!='Unclassified',] 
-  Thalamus_boxplot <- ggplot(data = Thalamus_Data, aes_string(x="Morel.Parcellations", y=v)) + geom_boxplot(outlier.colour = NULL,fill = "grey80") #+geom_point() + geom_jitter(position = position_jitter(width = .1))
-  Thalamus_boxplot <- Thalamus_boxplot + ggtitle(paste("Morel Atlas", v))  + theme_grey(base_size = 10)  + theme( axis.title.x=element_blank(), legend.position="none", axis.text = element_text(colour = "black"))
-  Thalamus_boxplot <- Thalamus_boxplot + ylim( 0, 1) 
-  plot(Thalamus_boxplot)
-  ggsave(filename = paste(v,'_morel_fn.png', sep=''), plot = Thalamus_boxplot, units = c("in"),width=6, height=2.5) 
-}
-
-Variables_to_plot <- c('WMD'  )
-for (v in Variables_to_plot){
-  
-  Thalamus_boxplot <- ggplot(data = Thalamus_Data, aes_string(x="Functional.Network", y=v)) + geom_boxplot(outlier.colour = NULL,fill = "grey80") #+geom_point() + geom_jitter(position = position_jitter(width = .1))
-  Thalamus_boxplot <- Thalamus_boxplot + ggtitle(paste("Functional Parcellation", v))  + theme_grey(base_size = 10)  + theme( axis.title.x=element_blank(), axis.title.y=element_blank(), axis.ticks.y=element_blank(),axis.text.y=element_blank(),legend.position="none", axis.text = element_text(colour = "black"))
-  Thalamus_boxplot <- Thalamus_boxplot + ylim( -2, 2) 
-  plot(Thalamus_boxplot)
-  ggsave(filename = paste(v,'_tha_fn.pdf', sep=''), plot = Thalamus_boxplot, units = c("in"),width=2.5, height=2.5) 
-  
-  Cortical_boxplot <- ggplot(data = Cortical_Data, aes_string(x="Functional.Network", y=v)) + geom_boxplot(outlier.colour = NULL, ,fill = "grey80") #+geom_point() + geom_jitter(position = position_jitter(width = .1))
-  Cortical_boxplot <- Cortical_boxplot + ggtitle(paste("Cortical ROI", v)) + theme_grey(base_size = 10) +  theme( axis.title.x=element_blank(), legend.position="none", axis.text = element_text(colour = "black"))
-  Cortical_boxplot <- Cortical_boxplot + ylim( -2, 2)
-  plot(Cortical_boxplot)
-  ggsave(filename = paste(v,'_cortical_box_consensus.pdf', sep=''), plot = Cortical_boxplot, units = c("in"),width=3, height=2.5) 
-  
-  Thalamus_boxplot <- ggplot(data = Thalamus_Data, aes_string(x="Anatomical.Parcellations", y=v)) + geom_boxplot(outlier.colour = NULL,fill = "grey80") #+geom_point() + geom_jitter(position = position_jitter(width = .1))
-  Thalamus_boxplot <- Thalamus_boxplot + ggtitle(paste("Oxford-FSL Atlas", v))  + theme_grey(base_size = 10)  + theme( axis.title.x=element_blank(), axis.title.y=element_blank(),axis.ticks.y=element_blank(), axis.text.y=element_blank(),legend.position="none", axis.text = element_text(colour = "black"))
-  Thalamus_boxplot <- Thalamus_boxplot + ylim( -2, 2) 
-  plot(Thalamus_boxplot)
-  ggsave(filename = paste(v,'_fsl_fn.pdf', sep=''), plot = Thalamus_boxplot, units = c("in"),width=2, height=2.5) 
-  
-  Thalamus_Data <- Thalamus_Data[Thalamus_Data$Morel.Parcellations!='Unclassified',] 
-  Thalamus_boxplot <- ggplot(data = Thalamus_Data, aes_string(x="Morel.Parcellations", y=v)) + geom_boxplot(outlier.colour = NULL,fill = "grey80") #+geom_point() + geom_jitter(position = position_jitter(width = .1))
-  Thalamus_boxplot <- Thalamus_boxplot + ggtitle(paste("Morel Atlas", v))  + theme_grey(base_size = 10)  + theme( axis.title.x=element_blank(), legend.position="none", axis.text = element_text(colour = "black"))
-  Thalamus_boxplot <- Thalamus_boxplot + ylim( -2, 2) 
-  plot(Thalamus_boxplot)
-  ggsave(filename = paste(v,'_morel_fn.png', sep=''), plot = Thalamus_boxplot, units = c("in"),width=7, height=2.5) 
-}
 
 
 #### patient stuff
@@ -166,44 +109,26 @@ plot(pt_pc_plot)
 
 #### AN, MD, PuM, Intra network stregnth 
 setwd('~/Google Drive/Projects/Thalamus-Rest/')
-CI_colors <- c("#0E6E6C", "#6B006C", "red", "yellow", "cyan", "brown", "#008100", "pink", "blue")
+CI_colors <- c("#6B006C", "red","yellow","cyan","blue", "brown", "#0E6E6C", "#008100", "pink")
 Nuclei_Data <- read.csv('NucleiNetworkStrength.csv')
-plot_Data <- Nuclei_Data[Nuclei_Data$Nuclei=='MD',] 
-n_plot <- ggplot(data = plot_Data, aes(x=factor(Network), y=Connectivity.Strength))
-n_plot <- n_plot + geom_bar(stat = "identity", aes(fill=Network)) + labs(y = "Z-Score") + theme_classic(base_size = 10)
-n_plot <- n_plot +scale_fill_manual(values=CI_colors ) 
-n_plot <- n_plot + theme(axis.title.x=element_blank(), axis.ticks.x=element_blank(), axis.text.x=element_blank(), axis.text = element_text(colour = "black"), legend.position="none")
-ggsave(filename = "MD_plot.pdf", plot = n_plot, units = c("in"),width=2, height=1.25) 
-plot(n_plot)
+Variables_to_plot <- c('MD', 'PuM', 'VL', 'An'  )
+for (v in Variables_to_plot){
+  plot_Data <- Nuclei_Data[Nuclei_Data$Nuclei==v,] 
+  n_plot <- ggplot(data = plot_Data, aes(x=factor(Network), y=Connectivity.Strength))
+  n_plot <- n_plot + geom_bar(stat = "identity", aes(fill=Network)) + labs(y = "Z-Score") + theme_classic(base_size = 10)
+  n_plot <- n_plot +scale_fill_manual(values=CI_colors ) 
+  n_plot <- n_plot + theme(axis.title.x=element_blank(), axis.ticks.x=element_blank(), axis.text.x=element_blank(), axis.text = element_text(colour = "black"), legend.position="none")
+  ggsave(filename = paste(v,'_plot.pdf', sep=''), plot = n_plot, units = c("in"),width=2, height=1.25) 
+  plot(n_plot)
+}
 
 
-### plot to compare within thalamus nodal role
-Variables_to_plot <- c('PC','within_PC','WMD', 'within_WMD', 'target_PC')
+### plot to compare with cog flexibility
+cog_plot <- ggplot(data = Thalamus_Data, aes(x=PC, y=cog))
+cog_plot <- cog_plot + geom_point()
+cog_plot <- cog_plot + geom_smooth(method=lm)
+plot(cog_plot)
 
-Thalamus_pc_plot <-ggplot(data = Thalamus_Data, aes(x=target_PC, y=PC))+geom_point()
-plot(Thalamus_pc_plot)
-
-### look at correlation between dataset
-Thalamus_pc_plot <-ggplot(data = Thalamus_Data, aes(x=MGH_NNC, y=NKI_NNC))+geom_point()
-plot(Thalamus_pc_plot)
-
-### look at correlation between thalamic voxel nodal property and its cortical target
-Thalamus_Target_Data = read.csv('Thalamus_target.csv', header=TRUE)
-total <- merge(Thalamus_Target_Data,Thalamus_Data,by="Voxel")
-
-Thalamus_pc_plot <-ggplot(data = total, aes(x=PC , y=Target.PC))+geom_point() + geom_jitter(position = position_jitter(width = .1)) +  scale_colour_manual(values=CI_colors) 
-Thalamus_pc_plot <- Thalamus_pc_plot + stat_smooth(method = 'lm', fill="blue", colour="darkblue", size=2) + theme_grey(base_size = 32) + ylab("Cortical ROIs' PC")+ xlab("Thalamic Voxels' PC")    
-plot(Thalamus_pc_plot)
-ggsave(filename = "Thalamocortical_PC.pdf", plot = Thalamus_pc_plot, units = c("in"),width=15, height=8) 
-
-Thalamus_wmd_plot <-ggplot(data = total, aes(x=WMD , y=Target.WMD))+geom_point() + geom_jitter(position = position_jitter(width = .1)) +  scale_colour_manual(values=CI_colors) 
-Thalamus_wmd_plot <- Thalamus_wmd_plot + stat_smooth(method = 'lm', fill="blue", colour="darkblue", size=2) + theme_grey(base_size = 32)+ ylab("Cortical ROIs' WMD")+ xlab("Thalamic Voxels' WMD") 
-plot(Thalamus_wmd_plot)
-ggsave(filename = "Thalamocortical_WMD.pdf", plot = Thalamus_wmd_plot, units = c("in"),width=15, height=8) 
-
-## look at tsnr
-Thalamus_boxplot <- ggplot(data = Thalamus_Data, aes_string(x="Associated.System", y="TSNR", fill="Associated.System")) + geom_boxplot() #+geom_point() + geom_jitter(position = position_jitter(width = .1))
-plot(Thalamus_boxplot)
 
 
   

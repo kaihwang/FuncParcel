9e8c50bee3d0e5882d72032db7a16520f0fc20bb
Kai Hwang <kai.hwang@gmail.com>
2016-09-28 10:23:07 -0700

    tet full corr.

--- analyze_thalamus_nodal_role.py
+++ analyze_thalamus_nodal_role.py
@@ -67,12 +67,12 @@ def ave_subject_nodal_role(dset, pattern):
 	aveM = np.array(sum(M_Sum)/len(files))
 
 
-def group_parcel_role(Datasets):
+def group_parcel_role(Datasets, thaparcel, Cortical_plus_thalamus_parcel_CI, Thalamus_Parcels, Thalamus_parcel_positions ):
 	'''calculate nodal metrics on group averaged matrices, unit of analysis at the thalamus is "parcel"'''
 
 	for dat in Datasets:
 		
-		Thalamocor_adj = np.loadtxt(Parcel_path + '/' + dat + 'Gordon_ThaWTA_avemat')
+		Thalamocor_adj = np.loadtxt(Parcel_path + '/' + dat + 'Gordon_' + thaparcel +'_avemat')
 		Cortical_adj = np.loadtxt(Parcel_path + '/' + dat + 'Gordon_333_cortical_avemat')
 
 		_, _, cost_thresholds = map_subcortical_cortical_targets(Thalamocor_adj, Cortical_ROIs, Thalamus_parcel_positions)
@@ -86,34 +86,34 @@ def group_parcel_role(Datasets):
 				Thalamus_parcel_positions, \
 				cost_thresholds)
 
-		file_path = path_to_data_folder + '/%savemat_tha_parcel_pcorr_PCs' %dat
+		file_path = path_to_data_folder + '/%savemat_tha_%sparcel_pcorr_PCs' %dat %thaparcel
 		save_object(PCs, file_path)
 
-		file_path = path_to_data_folder + '/%savemat_tha_parcel_pcorr_BNWRs' %dat
+		file_path = path_to_data_folder + '/%savemat_tha_%sparcel_pcorr_BNWRs' %dat %thaparcel
 		save_object(BNWRs, file_path)
 
-		file_path = path_to_data_folder + '/%savemat_tha_parcel_pcorr_NNCs' %dat
+		file_path = path_to_data_folder + '/%savemat_tha_%sparcel_pcorr_NNCs' %dat %thaparcel
 		save_object(NNCs, file_path)
 
-		file_path = path_to_data_folder + '/%savemat_tha_parcel_pcorr_WMDs' %dat
+		file_path = path_to_data_folder + '/%savemat_tha_%sparcel_pcorr_WMDs' %dat %thaparcel
 		save_object(WMDs, file_path)
 
-		file_path = path_to_data_folder + '/%savemat_tha_parcel_pcorr_bPCs' %dat
+		file_path = path_to_data_folder + '/%savemat_tha_%sparcel_pcorr_bPCs' %dat %thaparcel
 		save_object(bPCs, file_path)
 
-		file_path = path_to_data_folder + '/%savemat_tha_parcel_pcorr_meanPC' %dat
+		file_path = path_to_data_folder + '/%savemat_tha_%sparcel_pcorr_meanPC' %dat %thaparcel
 		save_object(mean_PC, file_path)
 
-		file_path = path_to_data_folder + '/%savemat_tha_parcel_pcorr_meanBNWR' %dat
+		file_path = path_to_data_folder + '/%savemat_tha_%sparcel_pcorr_meanBNWR' %dat %thaparcel
 		save_object(mean_BNWR, file_path)
 
-		file_path = path_to_data_folder + '/%savemat_tha_parcel_pcorr_meanNNC' %dat
+		file_path = path_to_data_folder + '/%savemat_tha_%sparcel_pcorr_meanNNC' %dat %thaparcel
 		save_object(mean_NNC, file_path)
 
-		file_path = path_to_data_folder + '/%savemat_tha_parcel_pcorr_meanWMD' %dat
+		file_path = path_to_data_folder + '/%savemat_tha_%sparcel_pcorr_meanWMD' %dat %thaparcel
 		save_object(mean_WMD, file_path)
 
-		file_path = path_to_data_folder + '/%savemat_tha_parcel_pcorr_meanbPC' %dat
+		file_path = path_to_data_folder + '/%savemat_tha_%sparcel_pcorr_meanbPC' %dat %thaparcel
 		save_object(mean_bPC, file_path)
 
 
@@ -163,7 +163,33 @@ def group_voxel_role(Datasets):
 		save_object(mean_WMD, file_path)
 
 		file_path = path_to_data_folder + '/%savemat_tha_nodal_pcorr_meanbPC' %dat
-		save_object(mean_bPCs, file_path)
+		save_object(mean_bPC, file_path)
+
+
+def group_fullcorr_vox():
+
+	M = average_corrmat('MGH*Gordon_333_plus_thalamus_ROIs_corrmat', np_txt=True, pickle_object=False)
+	M[np.isnan(M)] = 0  
+	M[Thalamus_voxel_positions,:][:,Thalamus_voxel_positions] = 0	
+	PCs = []
+	WMDs = []
+	for ix, c in enumerate(np.arange(0.01,0.16, 0.01)):
+		tM = bct.threshold_proportional(M, c, copy=True)
+		PCs += [bct.participation_coef(tM, Cortical_plus_thalamus_CI)]
+		WMDs += [bct.module_degree_zscore(tM, Cortical_plus_thalamus_CI)]
+
+
+	pPC =  pickle.load(open('/home/despoB/connectome-thalamus/Graph/MGH_avemat_tha_nodal_pcorr_meanPC','rb'))
+	pWMD = pickle.load(open('/home/despoB/connectome-thalamus/Graph/MGH_avemat_tha_nodal_pcorr_meanWMD','rb'))
+	fPC = np.mean(PCs,axis=0)[Thalamus_voxel_positions]
+	fWMD = np.mean(WMDs,axis=0)[Thalamus_voxel_positions]
+
+	Morel_mask = np.loadtxt('/home/despoB/connectome-thalamus/Thalamic_parcel/morel_mask')
+	mask = Morel_mask > 0
+
+
+	return M, PCs, WMDs	
+
 
 
 if __name__ == "__main__":
@@ -173,6 +199,51 @@ if __name__ == "__main__":
 ### group averaged
 	#Datasets =	['MGH_', 'NKI_645_', 'NKI_1400_'] 
 	Datasets =	['MGH_'] 
-	group_voxel_role(Datasets)
-	#group_parcel_role(Datasets)
+
+	## Anatomical parcel
+	MGH_Gordon_ThaFSL_avemat = average_corrmat('/home/despoB/kaihwang/Rest/NotBackedUp/ParMatrices/MGH*_Gordon_333_cortical_Thalamus_fslana_pcorr_mat')
+	np.savetxt('/home/despoB/kaihwang/Rest/Thalamic_parcel/MGH_Gordon_ThaFSL_avemat', MGH_Gordon_ThaFSL_avemat)
+
+	Thalamus_Parcels = np.array([1]*8) #an extra empty "400" thalamic parcel was included... 
+	Cortical_plus_thalamus_parcel_CI = np.append(Cortical_CI, Thalamus_Parcels)
+	Thalamus_parcel_positions = np.arange(len(Cortical_CI),len(Cortical_plus_thalamus_parcel_CI),1)
+
+	thaparcel = 'ThaFSL'
+	group_parcel_role(Datasets, thaparcel, Cortical_plus_thalamus_parcel_CI, Thalamus_Parcels, Thalamus_parcel_positions )
+
+
+	## Functional parcel
+	MGH_Gordon_ThaWTA_avemat = average_corrmat('/home/despoB/kaihwang/Rest/NotBackedUp/ParMatrices/MGH*_Gordon_333_cortical_ThaWTA_pcorr_mat')
+	np.savetxt('/home/despoB/kaihwang/Rest/Thalamic_parcel/MGH_Gordon_ThaWTA_avemat', MGH_Gordon_ThaWTA_avemat)
+
+	Thalamus_Parcels = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+	Cortical_plus_thalamus_CI = np.append(Cortical_CI, Thalamus_CIs)
+	Cortical_plus_thalamus_parcel_CI = np.append(Cortical_CI, Thalamus_Parcels)
+	Thalamus_parcel_positions = np.arange(len(Cortical_CI),len(Cortical_plus_thalamus_parcel_CI),1)
+
+	thaparcel = 'ThaWTA'
+	group_parcel_role(Datasets, thaparcel, Cortical_plus_thalamus_parcel_CI, Thalamus_Parcels, Thalamus_parcel_positions )
+
+
+	##Morel parcel
+	MGH_Gordon_ThaMorel_avemat = average_corrmat('/home/despoB/kaihwang/Rest/NotBackedUp/ParMatrices/MGH*_Gordon_333_cortical_Thalamus_Morel_consolidated_mask_pcorr_mat')
+	np.savetxt('/home/despoB/kaihwang/Rest/Thalamic_parcel/MGH_Gordon_ThaMorel_avemat', MGH_Gordon_ThaMorel_avemat)
+
+	Thalamus_Parcels = np.array([1]*16) #an extra empty "400" thalamic parcel was included... 
+	Cortical_plus_thalamus_CI = np.append(Cortical_CI, Thalamus_CIs)
+	Cortical_plus_thalamus_parcel_CI = np.append(Cortical_CI, Thalamus_Parcels)
+	Thalamus_parcel_positions = np.arange(len(Cortical_CI),len(Cortical_plus_thalamus_parcel_CI),1)
+
+	thaparcel = 'ThaMorel'
+	group_parcel_role(Datasets, thaparcel, Cortical_plus_thalamus_parcel_CI, Thalamus_Parcels, Thalamus_parcel_positions )	
+	
+	#voxel-wise
+	#group_voxel_role(Datasets)
+
+
+
+
+
+
+
 
